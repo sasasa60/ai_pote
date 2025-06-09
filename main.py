d8f44cfe75895512832e8dd3cbb5e72099f853a2
sasasa60 <cheahee0790@gmail.com>
2025-06-09 22:23:48 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from langchain_openai import ChatOpenAI
 import streamlit as st
 
 load_dotenv()
-
 api_key = os.getenv("OPENAI_API_KEY")
 
+if not api_key:
+    st.error("오류: OpenAI API 키가 설정되지 않았습니다.")
+    st.warning(".env 파일에 'OPENAI_API_KEY=YOUR_SECRET_KEY' 형태로 키가 올바르게 설정되었는지 확인해주세요.")
+    st.stop()
 chat_model = ChatOpenAI(api_key=api_key)
 
 st.title("인공지능 시인")
